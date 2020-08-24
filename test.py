86f77c46562e46fd865693326dd62d32c81dec22
juanlrcricci <juanjo.ricci@live.com.ar>
2020-08-24 18:34:28 -0300

    6to update

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import unittest
+from main import suma
 
 
 class Testing(unittest.TestCase):
-    pass
+    def test_suma(self):
+        self.assertEqual(suma(2, 2), 4)
 
 
 if __name__ == "__main__":
